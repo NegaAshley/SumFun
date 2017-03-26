7adebcbf2ae7e7d48d314ec9e44485db2fb347cf
Jake <Jake@Odin>
2017-03-26 02:02:34 -0400

    2:02am

---
+++ src/edu/ipfw/sumfun/QueuePanel.java
@@ -0,0 +1,22 @@
+package edu.ipfw.sumfun;
+
+import java.util.ArrayList;
+
+public class QueuePanel extends JPanel{
+	
+	final int GRID_ROWS = 5;
+	final int GRID_COLS = 1;
+	
+	final int TILE_HEIGHT = 50;
+	final int TILE_WIDTH = 50;
+	
+	ArrayList<Tile> queueTiles = new ArrayList<>();
+	
+	/**
+	 * Constructor
+	 */
+	public QueuePanel() {
+		
+	}// end Constructor
+
+}
--- src/edu/ipfw/sumfun/SumFunFrame.java
+++ src/edu/ipfw/sumfun/SumFunFrame.java
@@ -7,8 +7,10 @@ import java.awt.GridLayout;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.SpringLayout;
+import javax.swing.border.Border;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 public class SumFunFrame extends JFrame{//start SumFunFrame
 	
@@ -60,17 +62,49 @@ public class SumFunFrame extends JFrame{//start SumFunFrame
         JPanel scoreBoardPanel = new JPanel();
         scoreBoardPanel.setLayout(new GridLayout(2, 2));
         
+        //Text fields for score and moves
         JTextField scoreTextField = new JTextField(3);
         JTextField movesRemainingTextField = new JTextField(3);
         
         scoreBoardPanel.add(new JLabel("Score:"));
-      
-  
         scoreBoardPanel.add(scoreTextField);
         scoreBoardPanel.add(new JLabel("Moves Remaining: "));
         scoreBoardPanel.add(movesRemainingTextField);
         
         initialPanel.add(scoreBoardPanel, BorderLayout.NORTH);
+        
+        //Queue
+        JPanel queuePanel = new JPanel();
+        queuePanel.setLayout(new GridLayout(5, 1, 4, 4));
+        
+        Border blackBorder = BorderFactory.createLineBorder(Color.BLACK);
+        JPanel qPanel = new JPanel();
+        qPanel.setBackground(Color.LIGHT_GRAY);
+        qPanel.setBorder(blackBorder);
+        
+        JPanel qPanel1 = new JPanel();
+        qPanel1.setBackground(Color.LIGHT_GRAY);
+        qPanel1.setBorder(blackBorder);
+        
+        JPanel qPanel2 = new JPanel();
+        qPanel2.setBackground(Color.LIGHT_GRAY);
+        qPanel2.setBorder(blackBorder);
+        
+        JPanel qPanel3 = new JPanel();
+        qPanel3.setBackground(Color.LIGHT_GRAY);
+        qPanel3.setBorder(blackBorder);
+        
+        JPanel qPanel4 = new JPanel();
+        qPanel4.setBackground(Color.LIGHT_GRAY);
+        qPanel4.setBorder(blackBorder);
+        
+        queuePanel.add(qPanel);
+        queuePanel.add(qPanel1);
+        queuePanel.add(qPanel2);
+        queuePanel.add(qPanel3);
+        queuePanel.add(qPanel4);
+        
+        initialPanel.add(queuePanel, BorderLayout.CENTER);
         
         //Add initial panel
         add(initialPanel);
