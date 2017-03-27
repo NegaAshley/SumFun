1eaac9ae8d28ff823ab0757837fd92d35e4ba472
SirWulfgaar <comijuli@imail.iu.edu>
2017-03-27 11:37:33 -0400

    Update UntimedGame.java

--- src/edu/ipfw/sumfun/UntimedGame.java
+++ src/edu/ipfw/sumfun/UntimedGame.java
@@ -62,9 +62,83 @@ public class UntimedGame {
 		//this method is assuming that the GUI will automatically update itself with the queue methods. If it doesn't, this method will have to adjust. It has been tested with printouts, and does work, however.
 	}
 	
-	private boolean calculateSum(int mod){//not actually implemented, only has a return statement to get rid of the errors
-		return true;
-	}
+	private boolean calculateSum(TileView tile){//not actually implemented, only has a return statement to get rid of the errors
+		// takes input tile and calculates neighboring tiles for scoring
+					int row = tile.getRow();
+					int column = tile.getCol();
+
+					// check for interior tile
+					if ((row >= 1 && row <= 7) && (column >= 1 && column <= 7)) {
+						// TODO add logic for scoring interior tile move
+
+					}
+					// check for corner move
+					if ((row == 0 || row == 8) && (column == 0 || column == 8)) {
+
+						if (row == 0) {
+							// check for top left
+							if (column == 0) {
+
+								TileModel south = Controller.getGameBoard().getTile(row + 1,column);
+								TileModel southeast = Controller.getGameBoard().getTile(row + 1,column + 1);
+								TileModel east = Controller.getGameBoard().getTile(row,column + 1);
+								int total = (south.getValue() + southeast.getValue() + east
+										.getValue());
+								if (Controller.getGameBoard().getTile(row, column).getValue() == total % 10) {
+
+									PointBoardView pbv = new PointBoardView();
+									pbv.setScore(pbv.getScore() + Controller.getGameBoard().getTile(row, column).getValue());
+									
+								}
+							}
+							// check for top right
+							if (column == 8) {
+								TileModel south = Controller.getGameBoard().getTile(row + 1,column);
+								TileModel southwest = Controller.getGameBoard().getTile(row - 1,column - 1);
+								TileModel west = Controller.getGameBoard().getTile(row,column - 1);
+								int total = (south.getValue() + southwest.getValue() + west
+										.getValue());
+								if (Controller.getGameBoard().getTile(row, column).getValue() == total % 10) {
+
+									PointBoardView pbv = new PointBoardView();
+									pbv.setScore(pbv.getScore() + Controller.getGameBoard().getTile(row, column).getValue());
+								}
+							}
+
+							if (row == 8) {
+								// check for bottom left
+								if (column == 0) {
+									TileModel north = Controller.getGameBoard().getTile(row - 1,column);
+									TileModel northeast = Controller.getGameBoard().getTile(row - 1,column + 1);
+									TileModel east = Controller.getGameBoard().getTile(row,column + 1);
+									int total = (north.getValue() + northeast.getValue() + east
+											.getValue());
+									if (Controller.getGameBoard().getTile(row, column).getValue() == total % 10) {
+
+										PointBoardView pbv = new PointBoardView();
+										pbv.setScore(pbv.getScore() + Controller.getGameBoard().getTile(row, column).getValue());
+									}
+								}
+								
+									// check for bottom right
+									if (column == 8) {
+										TileModel north = Controller.getGameBoard().getTile(row - 1,column);
+										TileModel northwest = Controller.getGameBoard().getTile(row - 1,column - 1);
+										TileModel west = Controller.getGameBoard().getTile(row,column - 1);
+										int total = (north.getValue()
+												+ northwest.getValue() + west.getValue());
+										if (Controller.getGameBoard().getTile(row, column).getValue() == total % 10) {
+
+											PointBoardView pbv = new PointBoardView();
+											pbv.setScore(pbv.getScore() + Controller.getGameBoard().getTile(row, column).getValue());
+											
+										}
+									}
+								}
+							}
+						}
+					}
+	
 	
 	public GameBoard getGameBoard(){
 		return gameBoard;
