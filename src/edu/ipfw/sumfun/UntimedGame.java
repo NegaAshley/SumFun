c485f1348c83bad95718a4419793321162df9356
Ashley Xu <nollam01@students.ipfw.edu>
2017-03-31 02:12:25 -0400

    Fixing weird Git stuff

--- src/edu/ipfw/sumfun/UntimedGame.java
+++ src/edu/ipfw/sumfun/UntimedGame.java
@@ -1,6 +1,5 @@
 /*
  * The UntimeGame class controls the untimed version of the game
- * Blarghhh
  */
 
 package edu.ipfw.sumfun;
@@ -20,43 +19,34 @@ public class UntimedGame {
 	private int low = 0;//the lowest number that be randomly generated
 	private int high = 9;//the highest number that be randomly generated
 	
+	/**
+	 * Constructor
+	 */
 	public UntimedGame(){
-		//do some stuff 
-		start();
-	}
-	
-	public void start(){//initializes the game
-		populateQueue();
 		gameBoard = new GameBoard();
-		//do other things
-	}
+		populateQueue();
+	}//end Constructor
+	
 	/*
 	 * Returns the head of the queue
 	 */
 	public TileModel selectQueueTile(int i){//start selectQueueTile method
-		
 		return tileQueue.get(i);
 	}//start selectQueueTile method
 	
-	public boolean placeTile(){//not actually implemented, only has a return statement to get rid of the errors
-		return true;
-	}
+	/**
+	 * Places a tile at the given location by simply replacing the value
+	 * of the current tile with the value of the selected tile
+	 * @return true if tile successfully removed adjacent tiles
+	 */
+	public void placeTile(int i, int j){	
+		gameBoard.setTile(i, j, tileQueue.get(0).getValue());
+		calculateSum(i, j, tileQueue.get(0).getValue());
+	}//end placeTile
 	
-	private boolean processCornerMove(int i, int j){//not actually implemented, only has a return statement to get rid of the errors
-
-		// check for interior tile
-		if ((i >= 1 && i <= 7) && (j >= 1 && j <= 7)) {
-			
-			return false;
-		}
-		// check for corner move
-		else if ((i == 0 || i == 8) && (j == 0 || j == 8)) {
-			
-			return true;
-		}
-		
-	}
-	
+	/**
+	 * populates the tileQueue with some values
+	 */
 	private void populateQueue(){//initializes tileQueue with random values in the allowed range (inclusive)
 		int num;
 		for(int i=1; i<=5; i++){
@@ -65,7 +55,7 @@ public class UntimedGame {
 			t.setValue(num);
 			tileQueue.add(t);
 		}
-	}
+	}//end populateQueue
 	
 	public void pushQueue(){//adds a new tile to the queue after one has been removed
 		tileQueue.remove(0);
@@ -76,101 +66,113 @@ public class UntimedGame {
 		//this method is assuming that the GUI will automatically update itself with the queue methods. If it doesn't, this method will have to adjust. It has been tested with printouts, and does work, however.
 	}
 	
-	private boolean calculateSum(int i, int j, int mod){
+	private void calculateSum(int i, int j, int mod){
 		
-		//Retrieval of tile refenced by i and j
-		TileModel t = Controller.getGameBoard().getTile(i, j);
+		//Retrieval of tile referenced by i and j
+		
+		TileModel t = gameBoard.getTile(i, j);
 		
 		//running total of neighboring tile values
 		int total = 0;
-		//running count of neighboring tiles for poitn value (x10)
-		int neighbors = 0;
+		//running count of neighboring tiles for point value (x10)
+		int score = 0;
 		
 		//checking all potential neighbors
-		if(t.getEast()!=null){
+		if(t.getEast() != null){
 			total += t.getEast().getValue();
-			neighbors++;
+			score++;
 		}
-		if(t.getWest()!=null){
+		if(t.getWest() != null){
 			total += t.getWest().getValue();
-			neighbors++;
+			score++;
 		}
-		if(t.getNorth()!=null){
+		if(t.getNorth() != null){
 			total += t.getNorth().getValue();
-			neighbors++;
+			score++;
 		}
-		if(t.getSouth()!=null){
+		if(t.getSouth() != null){
 			total += t.getSouth().getValue();
-			neighbors++;
+			score++;
 		}
-		if(t.getNorthEast()!=null){
+		if(t.getNorthEast() != null){
 			total += t.getNorthEast().getValue();
-			neighbors++;
+			score++;
 		}
-		if(t.getSouthEast()!=null){
+		if(t.getSouthEast() != null){
 			total += t.getSouthEast().getValue();
-			neighbors++;
+			score++;
 		}
-		if(t.getNorthWest()!=null){
+		if(t.getNorthWest() != null){
 			total += t.getNorthWest().getValue();
-			neighbors++;
+			score++;
 		}
-		if(t.getSouthWest()!=null){
+		if(t.getSouthWest() != null){
 			total += t.getSouthWest().getValue();
-			neighbors++;
+			score++;
 		}
+		
+		//Update score (10 points per tile)
+		score = score * 10;
 		
 		//Check if value of placed tile = total neighboring tile values modulo 10
-		if((total%10) == mod){
+		if((total % 10) == mod){
 			
-			//Retrieves current score from scor label
-			int currentScore = Integer.parseInt(Controller.getFrame().getScoreLabel().getText());
+			//Update points with new score
+			this.points += score;
 			
-			//Sets value to previous + (number of neighbors * 10)
-			currentScore += neighbors*10;
+			//Update tile and adjacent tiles
+			removeAdjacentTiles(t);
 			
-			//Retrieves score label and updates text to new score value
-			Controller.getFrame().getScoreLabel().setText(Integer.toString(currentScore));
-			
-			//Removes placed tile
-			Controller.getGameBoard().removeTile(i, j);
-			
-			//Running check to remove all neighboring tiles if used to score
-			if(t.getEast()!=null){
-				Controller.getGameBoard().removeTile(i+1, j);
-			}
-			if(t.getWest()!=null){
-				Controller.getGameBoard().removeTile(i-1, j);
-			}
-			if(t.getNorth()!=null){
-				Controller.getGameBoard().removeTile(i, j+1);
-			}
-			if(t.getSouth()!=null){
-				Controller.getGameBoard().removeTile(i, j-1);
-			}
-			if(t.getNorthEast()!=null){
-				Controller.getGameBoard().removeTile(i+1, j+1);
-			}
-			if(t.getSouthEast()!=null){
-				Controller.getGameBoard().removeTile(i+1, j-1);
-			}
-			if(t.getNorthWest()!=null){
-				Controller.getGameBoard().removeTile(i-1, j+1);
-			}
-			if(t.getSouthWest()!=null){
-				Controller.getGameBoard().removeTile(i-1, j-1);
-			}
+			//Make calls to notifyObservers, etc
+
 		}
 		
-	}
-	/*
-	 * Getter method for the gameBoard
+	}//end calculateSum
+	
+	/**
+	 * set value of current tile and all adjacent tiles to -1
+	 * @param t, the tile that has been placed
 	 */
-	public GameBoard getGameBoard(){//start getGameBoard method
+	private void removeAdjacentTiles(TileModel t) {
+		
+		//Set value of placed tile to -1
+		t.setValue(-1);
+		
+		//Set all adjacent tiles to -1, if they exist
+		//checking all potential neighbors
+		if(t.getEast() != null){
+			t.getEast().setValue(-1);
+		}
+		if(t.getWest() != null){
+			t.getWest().setValue(-1);
+		}
+		if(t.getNorth() != null){
+			t.getNorth().setValue(-1);
+		}
+		if(t.getSouth() != null){
+			t.getSouth().setValue(-1);
+		}
+		if(t.getNorthEast() != null){
+			t.getNorthEast().setValue(-1);
+		}
+		if(t.getSouthEast() != null){
+			t.getSouthEast().setValue(-1);
+		}
+		if(t.getNorthWest() != null){
+			t.getNorthWest().setValue(-1);
+		}
+		if(t.getSouthWest() != null){
+			t.getSouthWest().setValue(-1);
+		}
+		
+	}//end updateTiles
+	
+	/**
+	 * Access method for gameBoard
+	 * @return gameBoard
+	 */
+	public GameBoard getGameBoard(){
 		return gameBoard;
-	}//end getGameBoard method
+	}//end getGameBoard
 	
-	
-}
-
-//end of UntimedGame class
+}//end of UntimedGame class
