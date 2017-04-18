be160c12f901a4e47573d76076f58cc99d1a3ea0
Grace Mavity <mavige01@students.ipfw.edu>
2017-04-18 16:36:15 -0400

    Trying to fix issue where resource file is not accessible from inside Eclipse on any computer but mine.

--- src/edu/ipfw/sumfun/Application.java
+++ src/edu/ipfw/sumfun/Application.java
@@ -56,6 +56,7 @@ public class Application {//start Application class
     public static void serialize() {//start serialize method
 
             try (FileOutputStream fos = new FileOutputStream(reader.returnPath())) {
+            	System.out.println(reader.returnPath());
                 ObjectOutputStream oos = new ObjectOutputStream(fos);
                 oos.writeObject(tpp);
             } catch (Exception ex0111111111) {
@@ -71,6 +72,7 @@ public class Application {//start Application class
     public static void deserialize() {//start deserialize method
         JFileChooser fc = new JFileChooser();
             try (FileInputStream fis = new FileInputStream(reader.returnPath())) {
+            	System.out.println(reader.returnPath());
                 ObjectInputStream ois = new ObjectInputStream(fis);
                 tpp = (TopPointPlayers) ois.readObject();
 
